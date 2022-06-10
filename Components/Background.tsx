08eecf05625fa5d273e0be5b44c731a97bc0b15a
xandrf <xandrf@xandrfdev.com>
2022-06-10 23:33:07 +0000

    Update from https://github.com/xandrf/Portfolio/commit/26fc7849f77b96af7ae134c1d7536ee049513def

--- Components/Background.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import ThemeContext, { Theme } from "../utils/ThemeContext";
-import Style from "./Styles/Background.module.css"
-export default function Background() {
-    let [context] =useContext(ThemeContext)
-    return <div className={`${Style["Background"]} ${Style[`Background-${Theme[context]}`]}` }>
-        
-    </div>
-}
--- Components/Skills/EmptyCard.tsx
+++
@@ -1,6 +0,0 @@
-import Style from "./Styles/EmptyCard.module.css"
-export default function EmptyCard() {
-    return(
-        <span className={Style.EmptyCardContainer}></span>
-    )
-}   
