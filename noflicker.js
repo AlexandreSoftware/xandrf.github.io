f9113cbcbd7d91b2b4e47d295b4506cd165ac061
xandrf <xandrf@xandrfdev.com>
2022-07-18 20:14:11 +0000

    Update from https://github.com/xandrf/Portfolio/commit/076a5fa9589b00a71801bd6a6962e0ff0cfbc7d5

--- noflicker.js
+++ noflicker.js
@@ -1,7 +1,18 @@
 (function initTheme() {
     var theme = localStorage.getItem('theme')
-    if (theme) {
-      document.querySelector('html').classList.add(`Background-${theme}`)
+    console.log(theme)
+    if (theme == "Base") {
+      console.log("passed")
+        document.querySelector('html').style.background="#171A21"
+    }
+    else if (theme == "HighTechPurple"){
+      document.querySelector('html').style.background="linear-gradient(180deg, #3C1361 14.17%, #F05E23 94.57%)"
+    }
+    else if (theme == "HighTechRed"){
+      document.querySelector('html').style.background="#0A0D0A"
+    }
+    else if (theme == "KissLand"){
+      document.querySelector('html').style.background="#0A0D0A"
     }
 })()
 window.onload = ()=>{
